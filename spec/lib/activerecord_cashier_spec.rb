530763d472e8c2eb5364e28f98970276fee2d06a
Alex Rozumey <brain-geek@yandex.ua>
2014-01-08 18:56:46 +0200

    Adding some 'subject' calls

--- spec/lib/activerecord_cashier_spec.rb
+++ spec/lib/activerecord_cashier_spec.rb
@@ -1,17 +1,17 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
-describe 'Cashier' do
+describe ActiverecordCashier do
   describe "#morph, #demorph" do
     it "should morph AR objects to strings" do
       source = [Page, Page.create!, 'page', 'page1', 'asdf']
-      ActiverecordCashier.morph(source).any?{|a| a.is_a? String}.should be_true
+      subject.morph(source).any?{|a| a.is_a? String}.should be_true
     end
 
     it "should be able to demorph objects correctly" do
       source = [Page, Page.create!, 'page', 'page1', 'asdf']
 
       #it does not lose anything after demorph. It may have some additional stuff
-      ActiverecordCashier.demorph(ActiverecordCashier.morph(source)).should include(*source)
+      subject.demorph(subject.morph(source)).should include(*source)
     end
   end
 end
